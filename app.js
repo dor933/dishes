fd48911fb44fdddee96c4c77fb20fbddfdb45ca8
dor raz <doratzabi1@gmail.com>
2022-10-31 18:24:41 +0200

    another commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const PORT=3000;
+const PORT=process.env.PORT
 const path=require('path');
 app.use(express.static(__dirname));
 app.use(express.json());
