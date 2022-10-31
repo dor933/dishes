bc4d4b0f4e051313a9d8567183619d3e04c84224
dor raz <doratzabi1@gmail.com>
2022-10-31 18:04:35 +0200

    init commit

---
+++ app.js
@@ -0,0 +1,129 @@
+const express=require('express');
+const app=express();
+const PORT=3000;
+const path=require('path');
+app.use(express.static(__dirname));
+app.use(express.json());
+
+
+
+
+const ingredientJson= [
+    {
+        id:1,
+        name:"brocooli",
+        imageurl:"https://st1.foodsd.co.il/Images/Products/large/uHmGri3t7b.jpg",
+        calories:34
+    }
+    ,
+    {
+        id:2,
+        name:"tomato",
+        imageurl:"https://st1.foodsd.co.il/Images/Products/large/hagiSJ2GI3.jpg",
+        calories:40
+        
+    } ,
+    {
+        id:3,
+        name:"soysauce",
+        imageurl:"https://cdn.shopify.com/s/files/1/0206/9470/products/10683_HFARM_49645309-3_grande.jpeg?v=1441105440",
+        calories:12
+    } ,
+
+    {
+        id:4,
+        name:"Nudels",
+        imageurl:"https://upload.wikimedia.org/wikipedia/commons/a/a8/Fresh_ramen_noodle_001.jpg",
+        calories:138
+    }
+
+
+]
+
+const Dishes= [
+    {
+        id:1,
+        dishname:"pad thai",
+        ingredients:[ingredientJson[0],ingredientJson[2],ingredientJson[3]],
+        time:20,
+        cookingmethod:"blabla",
+        imageurl:"https://d3o5sihylz93ps.cloudfront.net/wp-content/uploads/2020/05/13165500/IMG_0006-1.jpg",
+        calories:209
+    }
+]
+
+app.get('',(req,res) => {
+    res.sendFile(path.join(__dirname,'/index.html'))
+})
+
+app.get('/init',(req,res) => {
+    res.send({
+        ingredients:ingredientJson,
+        dishes:Dishes,
+        
+    })
+})
+
+app.post('/adddish',(req,res) => {
+    try {
+        const dish=req.body;
+        console.log(dish);
+        const dishingerconverted=[];
+        for(let i=0; i<dish.ingredients.length; i++){
+            dishingerconverted.push(ingredientJson.find(({id}) => id===dish.ingredients[i].id))
+        }
+
+        console.log('and this is dish converted:')
+        console.log(dishingerconverted)
+        Dishes.push({
+            id:dish.id,
+            dishname:dish.name,
+            ingredients:dishingerconverted,
+            time:dish.time,
+            cookingmethod:dish.cookingmethod,
+            imageurl:dish.imageurl,
+            calories:dish.calories
+
+
+        })
+
+        res.status(200).send({
+            ingerdient:ingredientJson,
+        dishes:Dishes
+        })
+    } catch(e) {
+        res.status(500).send();
+    }
+})
+
+app.post('/addingerdient',(req,res) => {
+
+    try{
+    const ingerdient=req.body;
+    console.log(req)
+    ingredientJson.push({
+        id:ingerdient.id,
+        name:ingerdient.name,
+        imageurl:ingerdient.imageurl,
+        calories:ingerdient.calories
+
+    })
+
+    res.status(200).send({
+        ingerdient:ingredientJson,
+        dishes:Dishes
+    });
+}
+catch(e) {
+    res.status(500).send();
+}
+})
+
+app.listen(process.env.PORT, () => {
+    console.log('app on the air in port '+PORT+'');
+})
+
+
+
+
+
---
+++ public/client.js
@@ -0,0 +1,230 @@
+
+let counter=4;
+let dishescounter=1;
+const disharray=[];
+const ingredientarray=[]
+
+
+    $("#addrecbtn").hover(function() {           
+        $(this).animate(
+            { deg: 360 },
+            {
+              duration: 1200,
+              step: function(now) {
+                $(this).css({ transform: 'rotate(' + now + 'deg)' });
+              }
+            }
+          );
+
+    })
+
+
+
+class ingredient {
+    id;
+    name;
+    imageurl;
+    calories;
+
+    constructor(id1,name1,imageurl1,calories1) {
+        this.id=id1;
+        this.name=name1;
+        this.imageurl=imageurl1;
+        this.calories=calories1;
+    }
+    
+    render() {
+        return `<div class="col-4 dishmaindiv">  <label class="checkbox-inline"> <input id=${this.id} type="checkbox" value="">Add</label>
+         <p class="dishespar"> ingerdient details: </p> <p class="dishespar"> <img src=${this.imageurl}> </p> <p class="dishespar"> dish name: ${this.name} </p> <p class="dishespar"> calories: ${this.calories} </p>  </div>`
+    }
+
+    rendertopopup() {
+        return `<div class="col-4 dishmaindiv"> 
+         <p class="dishespar"> ingerdient details: </p> <p class="dishespar"> <img src=${this.imageurl}> </p> <p class="dishespar"> dish name: ${this.name} </p> <p class="dishespar"> calories: ${this.calories} </p>  </div>`
+    }
+}
+
+class Dishrecipe {
+    id;
+    name;
+    ingredients;
+    time;
+    cookingmetohd;
+    imageurl;
+    calories;
+
+    constructor(id,name1,myingredients,time1,cookingmethod1,imageurl1,calories1){
+        this.id=id;
+        this.name=name1;
+        this.ingredients=myingredients;
+        this.time=time1;
+        this.cookingmetohd=cookingmethod1;
+        this.imageurl=imageurl1;
+        this.calories=calories1;
+    }
+
+    render() {
+        return `<div id=${this.id} class="col-2 dishmaindiv"> <p class="dishespar"> Dish recipe details: </p> <p class="dishespar"> <img src=${this.imageurl}> </p> <p class="dishespar"> dish name: ${this.name} </p> <p class="dishespar"> cooking method:blabla </p> <p class="dishespar"> Total cooking time:${this.time} </p> <p class="dishespar"> total calories:${this.calories} </p> <p class="dishespar"> <button type="button" class="btn btn-primary" onclick="showpopup(${this.id})"> Show ingredients </button> </p> </div>`
+    }
+
+    
+
+
+}
+
+function myinit(){
+fetch('http://localhost:3000/init').then((response) =>  {
+    response.json().then((data)=> {
+         renderpage(data)
+    })
+})
+
+}
+
+function renderpage({ingredients,dishes}) {
+
+ for(let i=0; i<dishes.length; i++) {
+    disharray.push(new Dishrecipe(dishes[i].id,dishes[i].dishname,dishes[i].ingredients,dishes[i].time,dishes[i].cookingmetohd,dishes[i].imageurl,dishes[i].calories))
+
+    document.getElementById('dishesdiv').innerHTML+=disharray[i].render();
+ }
+
+
+ for(let j=0; j<ingredients.length; j++){
+         let indegr=ingredients[j]
+          ingredientarray.push(new ingredient(indegr.id,indegr.name,indegr.imageurl,indegr.calories))
+         document.getElementById('indegdiv').innerHTML+=ingredientarray[j].render();
+     
+ }
+
+
+    
+    
+}
+
+$(document).ready(function() {
+    $("#addindbtn").click(function() {
+        $("#dishfrm").fadeOut();
+        $("#indegfrm").fadeIn();
+
+    }) })
+
+    $(document).ready(function() {
+        $("#closeind").click(function() {
+            $("#indegfrm").fadeOut();
+    
+        }) })
+
+        $(document).ready(function() {
+            $("#addrecbtn").click(function() {           
+                $("#indegfrm").fadeOut();
+                $("#dishfrm").fadeIn();
+        
+            })})
+
+        $(document).ready(function() {
+        $("#closedish").click(function() {$("#dishfrm").fadeOut();}) })
+
+        $(document).ready(function() {
+            $("#createind").click(function() {
+                const indegrentname=document.getElementById("indname").value;
+                const indimgurl=document.getElementById("indimg").value;
+                const indcal=document.getElementById("indcal").value;
+
+                addingredient(indegrentname,indimgurl,indcal)
+            })
+        })
+
+        $(document).ready(function() {
+            $("#createdish").click(function() {
+                const checkboxinger=document.querySelectorAll("input[type='checkbox']");
+                const dishingerarray=[];
+                const mydishname=document.getElementById("dishname").value;
+                const dishcookmet=document.getElementById("dishcookingmethod").value;
+                const dishcal=document.getElementById("dishcal").value;
+                const dishcookingtime=document.getElementById("dishcal").value;
+                const dishimgurl=document.getElementById("dishimg").value;
+                console.log(checkboxinger)
+
+
+                for(let i=0; i<checkboxinger.length; i++) {
+                    if(checkboxinger[i].checked){
+                        const ingertoadd=ingredientarray.find(x => x.id==checkboxinger[i].id)
+                        dishingerarray.push(ingertoadd);
+                    }
+                }
+
+                addnewdish(mydishname,dishingerarray,dishcookmet,dishcookingtime,dishcal,dishimgurl);
+            })
+        })
+
+
+function addnewdish(name,ingerdarray,cookingmethod,cookingtime,calor,dishimg) {
+
+    const dishtoadd=new Dishrecipe(++dishescounter,name,ingerdarray,cookingtime,cookingmethod,dishimg,calor);
+    fetch('http://localhost:3000/adddish', {
+        method:'post',
+        headers: {
+            "Content-Type": "application/json"
+          },
+        body:JSON.stringify(dishtoadd)}).then((response) => {
+            response.json().then((data)=> {
+                   ingredientarray=data.ingerdient
+                   disharray=data.dishes
+                   renderpage();
+
+            })
+        })
+
+}
+
+function showpopup(dishid) {
+
+    const modal=document.getElementById("myModal");
+    const innermodal=document.getElementById("modalinner");
+    innermodal.innerHTML="";
+    modal.style.display="block";
+    const relevantdish=disharray.find(x=> x.id==dishid)
+
+    for(let i=0; i<relevantdish.ingredients.length; i++){
+
+
+        const ingerdrelevant=ingredientarray.find(x=>x.id==relevantdish.ingredients[i].id)
+       innermodal.innerHTML+=ingerdrelevant.rendertopopup()
+
+    }
+}
+
+function closepopup(){
+    const modal=document.getElementById("myModal");
+    modal.style.display="none";
+
+}
+
+
+
+
+function addingredient(name,imageurl,calories) {
+
+    const newingeredient=new ingredient(++counter,name,imageurl,calories)
+    console.log(newingeredient)
+
+    
+    fetch('http://localhost:3000/addingerdient', {
+        method:'post',
+        headers: {
+            "Content-Type": "application/json"
+          },
+        body:JSON.stringify(newingeredient)}).then((response) => {
+            response.json().then((data)=> {
+                   ingredientarray=data.ingerdient
+                   disharray=data.dishes
+                   renderpage();
+
+            })
+        })
+    }
+
+    
+
+
